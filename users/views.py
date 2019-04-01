d0dfb778b82362d3de2e8b733480c72aff88c7a5
GM Ariful Islam <aarifshuvo@gmail.com>
2019-04-01 15:49:02 +0600

    done user registration from html page into db

--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.shortcuts import render, redirect
+from  .forms import UserRegisterForm
+from django.contrib import  messages
 
 def register(request):
     form = UserCreationForm()
     return render(request, 'users/register.html', { 'form' : form })
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form':form})
